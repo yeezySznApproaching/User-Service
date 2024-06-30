72d0268f19693c73041c260d3933cf736038fcd6
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-06-30 15:38:05 +0200

    updated the PORT to 3001 instead of 3000

--- user-service/index.js
+++ user-service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require ('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
