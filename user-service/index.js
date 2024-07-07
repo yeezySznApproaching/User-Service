c0f30dbbf65367afb60b0406fceda2278b3048d2
Jihad Ballout <148473573+yeezySznApproaching@users.noreply.github.com>
2024-07-07 15:32:07 +0200

    implemented a better connection logic in my indexjs file

--- user-service/index.js
+++ user-service/index.js
@@ -7,8 +7,11 @@ const PORT = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB', err);
 });
-
 const UserSchema = new mongoose.Schema ({
     name: String,
     email: String,
@@ -20,14 +23,26 @@ const User = mongoose.model('User', UserSchema);
 app.use(express.json());
 
 app.post('/users', async(req, res)=>{
+    try {
     const user = new User(req.body);
     await user.save();
     res.status(201).send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 app.get('/users/:id', async(req,res) =>{
-    const user = await User.findById(req.params.id);
-    res.status(200).send(user);
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            res.status(200).send(user);
+        } else {
+            res.status(404).send({ message: 'User not found'});
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 app.listen(PORT, ()=>{
